110dee2855434986713f16355906d759033690a6
Robert R. Henry <rrh@newrelic.com>
2014-11-17 15:42:34 -0800

    Don't do anythign special to squelch pre-jit work to avoid 32-bit address space, as needed for tsan, as tsan simply doesn't work on HHVM due to the presence of exceptions.

--- hphp/runtime/vm/jit/mc-generator.cpp
+++ hphp/runtime/vm/jit/mc-generator.cpp
@@ -1867,7 +1867,6 @@ MCGenerator::MCGenerator()
 }
 
 void MCGenerator::initUniqueStubs() {
-if (!RuntimeOption::EvalJit) { return; }
   // Put the following stubs into ahot, rather than a.
   CodeCache::Selector cbSel(CodeCache::Selector::Args(code).
                             hot(m_tx.useAHot()));
